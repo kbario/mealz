00690748478f424383f09428477567614f75f3f0
kbario <kylebario1@gmail.com>
2022-05-28 12:15:36 +0800

    added humourous placeholder info

---
+++ client/src/utils/randomIngreeds.js
@@ -0,0 +1,42 @@
+export const randomIngreeds = [
+  {
+    a: 4,
+    u: 'cloves',
+    i: 'garlic',
+  },
+  {
+    a: 500,
+    u: 'grams',
+    i: 'beef mince',
+  },
+  {
+    a: 3,
+    u: 'whole',
+    i: 'apples',
+  },
+  {
+    a: 125,
+    u: 'ml',
+    i: 'cream',
+  },
+  {
+    a: 8,
+    u: 'oz',
+    i: 'beef stock',
+  },
+  {
+    a: 1,
+    u: 'handful',
+    i: 'walnuts',
+  },
+  {
+    a: 3,
+    u: 'teaspoons',
+    i: 'cinnamon',
+  },
+  {
+    a: 1,
+    u: 'pinch',
+    i: 'baking soda',
+  },
+];
